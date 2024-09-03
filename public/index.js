802f695a283522e8489cc9c6934b41552f4e2c6e
antoniogui33 <156611747+antoniogui33@users.noreply.github.com>
2024-09-03 18:15:24 -0300

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const portaDeEntrada = process.env.PORT || 3000;
 
-
+//não esquece de ligar a porcaria do servidor,
+//e abra o navegador com o localhost e não pelo live server.
 
 const servidor = http.createServer((pedido, resposta) => {
     if (pedido.url === '/') {
@@ -30,6 +31,129 @@ const servidor = http.createServer((pedido, resposta) => {
             return
         })
     }
+    if (pedido.url === '/saude') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/saude.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/meioAmbiente') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/meioAmbiente.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/seguranca') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/seguranca.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/economia') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/economia.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/cultura') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/cultura.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/gestao') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/gestao.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/politicaInterna') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/politicaInterna.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/politicaExterna') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/politicaExterna.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/qualidadeDeVida') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/qualidadeDeVida.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/exercito') {
+        fs.readFile(path.resolve(__dirname, '../frontend/view/exercito.html'), (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Desculpe, não foi possivel encontrar sua pesquisa, recarregue a pagína ou tente novamente mais tarde!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+
+    //A partir da linha 156, entramos na area de CSS
+
     if (pedido.url === "/css/index.css") {
         fs.readFile(path.resolve(__dirname, '../frontend/view/css/index.css'), (erro, dadosDoArquivo) => {
             if (erro) {
@@ -42,7 +166,6 @@ const servidor = http.createServer((pedido, resposta) => {
             return
         })
     }
-    
 });
 
 servidor.listen(portaDeEntrada, '0.0.0.0');
